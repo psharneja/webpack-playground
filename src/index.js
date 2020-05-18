be42a1a791a66ccab1d63b0bc306baff4787619a
Prabhsimran Singh (Simar Harneja) <psharneja@gmail.com>
2020-05-18 22:10:55 +0530

    default build of bootstarp

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button';
 import Heading from './components/heading/heading'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
 
 const heading = new Heading();
 heading.render();
